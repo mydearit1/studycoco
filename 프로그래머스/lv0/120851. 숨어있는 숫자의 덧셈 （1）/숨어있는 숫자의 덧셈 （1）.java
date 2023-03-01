ca86607c85088af29b677387a3af39f60b375f5e
mydearit1 <124052283+mydearit1@users.noreply.github.com>
2023-03-01 22:02:10 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 0.17 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        String str = my_string.replaceAll("[^0-9]","");
+
+        for(char ch : str.toCharArray()) {
+            answer += Character.getNumericValue(ch);
+        }
+
+        return answer;
+    }
+}
